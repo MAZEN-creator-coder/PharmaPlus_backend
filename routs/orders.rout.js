4add5f3760ed106dbb3d1740d1c47db39d017de1
MariamRiad555 <mariamriad919@gmail.com>
2025-11-30 17:05:30 +0200

    mmmm

--- routs/orders.rout.js
+++ routs/orders.rout.js
@@ -5,11 +5,17 @@ const verifyToken = require("../middleware/verifytoken");
 const router = express.Router();
 
 router.post("/", verifyToken, controller.createOrder);
+
 router.get("/", verifyToken, controller.getOrders);
-router.get("/:id", verifyToken, controller.getOrderById);
+
 router.get("/user/:userId", verifyToken, controller.getOrdersByUser);
+
 router.get("/pharmacy/:pharmacyId", verifyToken, controller.getOrdersByPharmacy);
+
+router.get("/:id", verifyToken, controller.getOrderById);
+
 router.put("/:id/status", verifyToken, controller.updateOrderStatus);
+
 router.delete("/:id", verifyToken, controller.deleteOrder);
 
 module.exports = router;
